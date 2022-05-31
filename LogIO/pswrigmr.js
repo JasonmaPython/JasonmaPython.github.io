8b7642f986fafabd975d92bbbf3575108194c3d2
JasonmaPython <jerry96522@126.com>
2022-05-31 21:59:43 +0800

    Create pswrigmr.js

---
+++ LogIO/pswrigmr.js
@@ -0,0 +1,11 @@
+canvs = document.getElementById("cvsimg").getContext("2d")
+class letter {
+    a() {
+        canvs.moveTo(300, 300);
+        canvs.fillStyle = "black";
+        canvs.font = '20px "Arial"';
+        canvs.textBaseline = "bottom";
+        canvs.textAlign = "left";
+        canvs.fillText
+    }
+}
