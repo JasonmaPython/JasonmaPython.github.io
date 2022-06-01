30e0d45be3276f62cbe2cfd11827768abb6971f5
JasonmaPython <jerry96522@126.com>
2022-06-01 17:32:52 +0800

    Update pswrigmr.js

--- LogIO/pswrigmr.js
+++ LogIO/pswrigmr.js
@@ -1,15 +1,12 @@
 canvs = document.getElementById("cvsimg").getContext("2d")
 class letter {
     constructor(pos){
-        if (pos == 0) {
-            this.real=[10,50]
-        }
-    }
-    a() {
+        this.real=[10+pos*74,]
         canvs.fillStyle = "black";
         canvs.font = '100px "Arial"';
+    }
+    a() {
         canvs.fillText("A",this.real[0],this.real[1])
     }
 }
-letr = new letter(0);
-letr.a();
+letter(0).a();
